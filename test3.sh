e7c70b2ff8de66d28f2172d4dd690ce1fa64d4e3
HerKaen <nicoglenz@arcor.de>
2016-12-05 14:46:16 +0100

    Bashing

--- test3.sh
+++ test3.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-while [[ $eingabe -ne 1 ]]
-do
-	read -p "Mach Eingabe: " eingabe
-	echo $eingabe
-done
+if ! [[ $1 ]] ; then
+	echo "Eingabe fehlt!"
+else
+
+topic=$1
+
+if ! [ -d "/home/nico/$topic" ] ; then
+	mkdir /home/nico/$topic
+else
+	cd /home/nico/$topic
+fi
+
+read -p "Geben Sie Notizen an: " notiz
+
+datum=$(date +"%d.%m.%Y - %H:%M:%S")
+
+echo "Datum: " $datum" | Notiz: " $notiz  >> /home/nico/$topic/notes.txt
+
+fi
